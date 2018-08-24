7a7c6fd365bf69061432820b2b7c831e9a3a8e79
anthony62490 <anthonyjmagno@gmail.com>
2018-08-24 17:29:51 -0500

    End of Assignment. PUT request working on backend. Moving to the front.

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component
           <Dashboard 
             productList={this.state.productList}
             updateList={this.getUpdatedInventoryList}/>
+            
           <Form 
             updateList={this.getUpdatedInventoryList}
             currentlyEditingID={this.state.currentlyEditingID}/>
